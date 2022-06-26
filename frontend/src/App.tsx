9a454679f64e3ac6975409a31c91fbf5ea1e5353
devfrank9 <devfrank9@gmail.com>
2022-06-26 21:37:00 +0900

    Feat: "라우터 추가"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
