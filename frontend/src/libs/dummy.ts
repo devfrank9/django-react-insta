b9351eebb05792e36a25b86415310f88e87ae9a8
devfrank9 <devfrank9@gmail.com>
2022-07-09 20:45:29 +0900

    더미데이터

---
+++ frontend/src/libs/dummy.ts
@@ -0,0 +1,76 @@
+export const Board = [
+  {
+    id: 1,
+    title: "for life",
+    picture: "test1",
+    comment: 5,
+    favor: 3,
+  },
+  {
+    id: 2,
+    title: "my lifes for aier",
+    picture: "test2",
+    comment: 2,
+    favor: 5,
+  },
+  {
+    id: 3,
+    title: "개추어 크래용",
+    picture: "test3",
+    comment: 10,
+    favor: 5,
+  },
+  {
+    id: 4,
+    title: "트롤킹",
+    picture: "test4",
+    comment: 10,
+    favor: 15,
+  },
+  {
+    id: 5,
+    title: "리그오브레전드",
+    picture: "test5",
+    comment: 13,
+    favor: 2,
+  },
+];
+
+export const Comment = [
+  {
+    id: 1,
+    refId: 1,
+    username: "이렐리아",
+    content: "아이오니아를 위하여",
+  },
+  {
+    id: 2,
+    refId: 1,
+    username: "다리우스",
+    content: "녹서스!",
+  },
+  {
+    id: 3,
+    refId: 1,
+    username: "스웨인",
+    content: "비둘기야 밥먹자",
+  },
+  {
+    id: 4,
+    refId: 2,
+    username: "아이유",
+    content: "이른아침 좋은 날들",
+  },
+  {
+    id: 5,
+    refId: 2,
+    username: "권지용",
+    content: "개추어 크래용",
+  },
+  {
+    id: 6,
+    refId: 2,
+    username: "패배",
+    content: "패배배배배",
+  },
+];
